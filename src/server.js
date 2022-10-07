f0546d91c443daad26140358ff8bb33e4acc6e92
Mark Wlodawski <expresso@expresso.local>
2022-10-07 15:00:16 -0400

    Add Relationships feature

--- src/server.js
+++ src/server.js
@@ -1,15 +1,26 @@
-import { createServer, Model } from "miragejs";
+import { createServer, Model, hasMany, belongsTo } from "miragejs";
 
 export default function () {
   createServer({
     models: {
-      reminder: Model,
+      list: Model.extend({
+        reminders: hasMany(),
+      }),
+      reminder: Model.extend({
+        list: belongsTo(),
+      }),
     },
 
     seeds(server) {
       server.create("reminder", { text: "Walk the dog" });
       server.create("reminder", { text: "Take out the trash" });
       server.create("reminder", { text: "Work out" });
+
+      let homeList = server.create("list", { name: "Home" });
+      server.create("reminder", { list: homeList, text: "Do taxes" });
+
+      let workList = server.create("list", { name: "Work" });
+      server.create("reminder", { list: workList, text: "Visit bank" });
     },
 
     routes() {
@@ -19,8 +30,25 @@ export default function () {
 
       this.post("/api/reminders", (schema, request) => {
         let attrs = JSON.parse(request.requestBody);
-
+        console.log(attrs);
         return schema.reminders.create(attrs);
+      });
+
+      this.delete("/api/reminders/:id", (schema, request) => {
+        let id = request.params.id;
+
+        return schema.reminders.find(id).destroy();
+      });
+
+      this.get("/api/lists", (schema, request) => {
+        return schema.lists.all();
+      });
+
+      this.get("/api/lists/:id/reminders", (schema, request) => {
+        let listId = request.params.id;
+        let list = schema.lists.find(listId);
+
+        return list.reminders;
       });
     },
   });
